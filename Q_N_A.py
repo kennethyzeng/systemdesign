9ccd89efd2badedf9be4d2c01b333df155a15aad
kennethyzeng <kennethyzeng@gmail.com>
2024-06-03 22:06:57 -0700

    add

---
+++ Q_N_A.py
@@ -0,0 +1,33 @@
+#1 Distributed Storage solutions? 
+What is Hot/cool/cold storage?
+
+
+#2 backup
+what is cold/warm/hot standby?
+
+
+#3 Search and information retrieval 
+What is forward index and inverted index ?
+start with a forward index of keywords in each document 
+tuple with document ID, position 
+
+then generate an inverted index that maps keywords to documents 
+sometime, those cocument need to be ranked
+
+#4 TF-IDF: document search 
+Term frequency and inverse document frequency 
+
+Term frequqncy * inverse document frequency 
+
+#5 pageRank: searching the web 
+pages's backlinks 
+anchor text for those links 
+
+A given backlin is weighted by how many other links are on the page it's coming from
+
+#6 working with big data 
+-message Queue as a scaling tool 
+act like a buffer between two systems
+
+-need to study more about "how spark work"  
+-
